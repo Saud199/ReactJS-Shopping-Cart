e953e3b05c8a26d1692934cb64bcd99603348bf3
Saud199 <qsa199@hotmail.com>
2020-12-06 16:52:16 +0500

    second commit

--- src/components/itemsList.js
+++ src/components/itemsList.js
@@ -6,7 +6,8 @@ class ItemsList extends Component {
     constructor() {
         super();
         this.state = {
-            products: Items.items
+            products: Items.items,
+            searchText: ''
         }
     }
 
@@ -25,6 +26,7 @@ class ItemsList extends Component {
 
     addData() {
         const { products } = this.state;
+
         var obj = {
             name: "Item 5",
             price: "500",
@@ -35,11 +37,72 @@ class ItemsList extends Component {
         console.log(products)
     }
 
+    onChange = e => {
+        this.setState({ searchText: e.target.value })
+    }
+
     render() {
+        const { products } = this.state;
         return (
             <div>
-                Hi
-                <button onClick={() => this.addData()}>add</button>
+                <nav className="navbar navbar-expand-lg fixed-top" style={{ textAlign: 'center', float: 'none', display: 'inline-block' , backgroundColor : '#007BFF'}}>
+                    {/* <img src={Logo} className="card-img-top" style={{width: '300px' , height : '80px'}} alt="Logo" /> */}
+                    <div style={{ float: "right" }}>
+                        <i className="fa" style={{ fontSize: "34px" }}>&#xf07a;</i>
+                        <span className='badge badge-warning' id='lblCartCount'> 0 </span>
+                    </div>
+                </nav>
+
+                <br /><br /><br /><br />
+
+                <div className="container">
+
+                    <br />
+                    <div className="row" style={{ textAlign: "center" }}>
+                        <div className="col-sm-1">
+
+                        </div>
+                        <div className="col-sm-9">
+                            <input type="text" placeholder="Search Products" onChange={this.onChange} className="form-control animate__animated animate__zoomInDown" style={{ margin: '2px' }} ></input>
+                        </div>
+                    </div>
+                    <br />
+
+                    <div className="row">
+                        {
+                            products.map((val, ind) => {
+                                const { searchText } = this.state;
+
+                                if (searchText !== "" && val.name.toLowerCase().indexOf(searchText.toLowerCase()) === -1) {
+                                    return null;
+                                }
+
+                                return (
+                                    <div className="card mx-2 my-2 shadow-lg p-3 mb-5 bg-white rounded" key={ind} style={{ width: '345px', alignItems: 'center' }}>
+
+                                        {/* src={require('../images/colgate.jpg').default} */}
+                                        <img src={val.imageURL} className="card-img-top" style={{ height: '240px', width: '240px' }} alt="product image" />
+                                        <div className="card-body" style={{ backgroundColor: '#a6b0bf', width: '100%', textAlign: 'center' }}>
+                                            <h5 className="card-title">{val.name}</h5>
+                                            <p className="card-text">${val.price}</p>
+
+                                            {(val.availability == 'In Stock') &&
+                                                <div>
+                                                    <p className="card-text" style={{ color: "green", fontWeight: 'bold' }}>{val.availability}</p>
+                                                    <button type="button" style={{ width: '120px' }} className="btn btn-primary">Add to Cart</button>
+                                                </div>}
+
+                                            {(val.availability == 'Out of Stock') &&
+                                                <p className="card-text" style={{ color: "red", fontWeight: 'bold' }}>{val.availability}</p>}
+
+                                        </div>
+                                    </div>
+                                );
+                            })
+                        }
+                    </div>
+                </div>
+
             </div>
         );
     }
