265cce553592718db2208e05d74546b568014af2
Saud199 <qsa199@hotmail.com>
2020-12-08 11:24:33 +0500

    sixth commit

--- src/components/shoppingCart.js
+++ src/components/shoppingCart.js
@@ -34,44 +34,49 @@ class ShoppingCart extends Component {
                 <br />
 
                 <div className="container">
-                    <table className="table table-hover">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Item</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Quantity</th>
-                                <th scope="col">Unit Price</th>
-                                <th scope="col">Total Price</th>
-                                <th scope="col"> </th>
-                            </tr>
-                        </thead>
-                        {this.props.cartItems.map((val, ind) => {
-                            return (
-                                <tbody key={ind}>
-                                    <tr>
-                                        <th scope="row" style={{ verticalAlign: 'middle' }}>{ind + 1}</th>
-                                        <td style={{ verticalAlign: 'middle' }}><img src={val.imageURL} style={{ width: '80px', height: '80px' }} alt="product image" /></td>
-                                        <td style={{ verticalAlign: 'middle' }}>{val.name}</td>
-                                        <td style={{ verticalAlign: 'middle' }}>
-                                            <button type="button" style={{ marginRight: '5px', width: '40px' }} className="btn btn-light"><b>-</b></button>
-                                            {val.quantity}
-                                            <button type="button" style={{ marginLeft: '5px', width: '40px' }} className="btn btn-light"><b>+</b></button>
-                                        </td>
-                                        <td style={{ verticalAlign: 'middle' }}>${val.price}</td>
-                                        <td style={{ verticalAlign: 'middle' }}>${val.total_price}</td>
-                                        <td style={{ verticalAlign: 'middle' }}><button type="button" onClick={() => this.removeItem(ind)} className="btn btn-primary">Remove</button></td>
-                                    </tr>
-                                </tbody>
-                            );
-                        })}
-                    </table>
+                    <div className="table-responsive">
+                        <table className="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Item</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Quantity</th>
+                                    <th scope="col">Unit Price</th>
+                                    <th scope="col">Total Price</th>
+                                    <th scope="col"> </th>
+                                </tr>
+                            </thead>
+                            {this.props.cartItems.map((val, ind) => {
+                                return (
+                                    <tbody key={ind}>
+                                        <tr>
+                                            <th scope="row" style={{ verticalAlign: 'middle' }}>{ind + 1}</th>
+                                            <td style={{ verticalAlign: 'middle' }}><img src={val.imageURL} style={{ width: '80px', height: '80px' }} alt="product image" /></td>
+                                            <td style={{ verticalAlign: 'middle' }}>{val.name}</td>
+                                            <td style={{ verticalAlign: 'middle' }}>
+                                                <button type="button" style={{ marginRight: '5px', width: '40px' }} className="btn btn-light"><b>-</b></button>
+                                                {val.quantity}
+                                                <button type="button" style={{ marginLeft: '5px', width: '40px' }} className="btn btn-light"><b>+</b></button>
+                                            </td>
+                                            <td style={{ verticalAlign: 'middle' }}>${val.price}</td>
+                                            <td style={{ verticalAlign: 'middle' }}>${val.total_price}</td>
+                                            <td style={{ verticalAlign: 'middle' }}><button type="button" onClick={() => this.removeItem(ind)} className="btn btn-primary">Remove</button></td>
+                                        </tr>
+                                    </tbody>
+                                );
+                            })}
+                        </table>
+                    </div>
+
+                    
+
                 </div>
 
                 <br /><br /><br />
-                <nav className="navbar navbar-expand-lg fixed-bottom" style={{ textAlign: 'center', float: 'none', display: 'inline-block', backgroundColor: '#FFFFFF', borderTop : 'solid 1px black' }}>
-                    <h4>Grand Total : $2500</h4>
-                </nav>
+                    <nav className="navbar navbar-expand-lg fixed-bottom" style={{ textAlign: 'center', float: 'none', display: 'inline-block', backgroundColor: '#FFFFFF', borderTop: 'solid 1px black' }}>
+                        <h4>Grand Total : $2500</h4>
+                    </nav>
 
             </div>
         );
