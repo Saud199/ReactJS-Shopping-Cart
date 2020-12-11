47d46a4719689c44f2a35fdfff71b3c88eaa0bd9
Qazi Saud Alam <qsa199@hotmail.com>
2020-12-11 12:32:49 +0500

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,33 @@
 import rootReducer from './reducer';
-import {createStore, applyMiddleware} from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
-const store = createStore(
-    rootReducer,
-    {},
-    applyMiddleware(thunk)
-);
+const persistConfig = {
+    key: 'root',
+    storage,
+}
 
-export default store;
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = createStore(persistedReducer, applyMiddleware(thunk));
+const persistor = persistStore(store);
+export { store, persistor };
+
+
+
+//       ***** OLD STORE *****
+
+// import rootReducer from './reducer';
+// import {createStore, applyMiddleware} from 'redux';
+// import thunk from 'redux-thunk';
+
+// const store = createStore(
+//     rootReducer,
+//     {},
+//     applyMiddleware(thunk)
+// );
+
+// export default store;
