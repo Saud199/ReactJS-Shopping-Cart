15933ced39afcf97e6d37667b7e87436b1f6026d
Qazi Saud Alam <qsa199@hotmail.com>
2020-12-11 12:31:01 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ItemsList from './components/itemsList.js';
 import ShoppingCart from './components/shoppingCart.js'
 import { Provider } from 'react-redux';
-import store from './store';
+import { store, persistor } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={ItemsList} />
-            <Route exact path="/cart" component={ShoppingCart} />
-          </Switch>
-        </Router>
+        <PersistGate loading={null} persistor={persistor}>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={ItemsList} />
+              <Route exact path="/cart" component={ShoppingCart} />
+            </Switch>
+          </Router>
+        </PersistGate>
       </Provider>
     );
   }
